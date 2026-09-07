dcae8cbc85cdf46d49089eaf34d48915c280515b
Adrian <adrian.montano.c@ucb.edu.bo>
2026-09-07 16:46:56 -0400

    feat: agregar la clase Biblioteca Service con datos suficientes

---
+++ BibliotecaService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BibliotecaService : IRepositorio<Libro>
+{
+    private List<Libro> libros;
+    private List<Usuario> usuarios;
+    private List<Prestamo> prestamos;
+
+    private string[] categorias = {"Novela","Ciencia","Historia","Tecnologia","Educacion"};
+
+    public BibliotecaService()
+    {
+        libros = new List<Libro>();
+        usuarios = new List<Usuario>();
+        prestamos = new List<Prestamo>();
+    }
+}
